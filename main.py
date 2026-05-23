2cea75c72f7d3f120d44dd958b2494294fd1580e
islombek202 <islombekkarimov246@gmail.com>
2026-05-23 18:51:14 +0500

    Add initial bot implementation and configuration
    - Created main.py with bot commands and handlers
    - Added .gitignore to exclude environment files and cache
    - Updated requirements.txt for necessary dependencies

---
+++ main.py
@@ -0,0 +1,102 @@
+import telebot
+from telebot import types
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    keyboard=types.ReplyKeyboardMarkup()
+    btn1=types.KeyboardButton('About me')
+    btn2=types.KeyboardButton('Contact')
+    btn3=types.KeyboardButton('Services')
+    btn4=types.KeyboardButton('Portfolio')
+    keyboard.add(btn1, btn2,)
+    keyboard.add(btn3, btn4)
+    text = """
+✨ Salom 👋🏻
+
+🤖 Botimizga xush kelibsiz!
+
+⚡ Siz uchun tezkor va qulay xizmat tayyor 🚀
+
+📝 Kerakli buyruq yoki matnni yuboring.
+
+💎 Smart • Fast • Easy
+
+🔥🌐⚡
+"""
+    bot.send_message(message.chat.id,"""
+✨ Salom 👋🏻
+
+🤖 Botimizga xush kelibsiz!
+
+⚡ Siz uchun tezkor va qulay xizmat tayyor 🚀
+
+📝 Kerakli buyruq yoki matnni yuboring.
+
+💎 Smart • Fast • Easy
+
+🔥🌐⚡
+""",reply_markup=keyboard)
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     if message.text == 'About me':
+#         bot.send_message(message.chat.id, "Men Islombek Karimov.\n men 2009-yil tug'ilganman")
+#     elif message.text == 'Contact':
+#         bot.send_message(message.chat.id, "Telefon raqam: +998995767309\nEmail: islombekkarimov246@gmail.com")
+
+@bot.message_handler(func=lambda message: message.text == 'About me')
+def aboutme_handler(message):
+    bot.send_message(message.chat.id,  """
+👨‍💻 Islombek
+
+🎂 25-April, 2009
+🏫 School: 32-maktab
+
+🚀 Python programmer
+🌐 Web developer
+🤖 Telegram bot creator
+
+⚡ Tech lover
+💎 Always improving skills
+
+""")
+@bot.message_handler(func=lambda message: message.text == 'Contact')
+def contact_handler(message):
+  
+    bot.send_message(message.chat.id,"""
+📞 Telefon raqam:
+📱 +998 99 5766 73 09
+
+📧 Email:
+✉️ islombekkarimovvv246@gmail.com
+
+
+━━━━━━━━━━━━━━━
+🤖 Telegram Bot Contact Info
+✨ Tez aloqa uchun yuqoridagi ma’lumotlardan foydalaning!""")
+@bot.message_handler(func=lambda message: message.text == 'Contact')
+def contact_handler(message):
+    keyboard=types.InlineKeyboardMarkup()
+    link1=types.InlineKeyboardButton("Telegram", url="https://t.me/@karimovkarimov_bot")
+    link2=types.InlineKeyboardButton("E-mail", url="mailto:islombekkarimovvv246@gmail.com")
+    link3=types.InlineKeyboardButton("Phone", url="tel:+998995767309")
+    link4=types.InlineKeyboardButton("LinkedIn", url="https://www.linkedin.com/in/islombek-karimov-b610a5408/")
+    keyboard.add(link1, link2)
+    keyboard.add(link3, link4)
+    bot.send_message(message.chat.id, "Yuqoridagi ma'lumotlardan foydalanib tezda bog'lanishingiz mumkin!", reply_markup=keyboard)
+      
+@bot.message_handler(func=lambda message: message.text == 'Services')
+def services_handler(message):
+    bot.send_message(message.chat.id, "Bu qism tez orada qo'shiladi...")
+@bot.message_handler(func=lambda message: message.text == 'Portfolio')
+def portfolio_handler(message): 
+    bot.send_message(message.chat.id, "Bu qism tez orada qo'shiladi...")
+bot.infinity_polling()
